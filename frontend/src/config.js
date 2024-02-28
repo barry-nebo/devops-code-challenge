cdec32d38c3b336920264476710d043bbcbca2f5
shittu barry <adebaresulaimon04@gmail.com>
2024-02-28 17:16:23 +0100

    updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // export const API_URL = 'http://localhost:8080/'
 
-export const API_URL = 'http://54.227.146.135:8080/'
+export const API_URL = 'http://18.234.69.52:8080/'
 export default API_URL
